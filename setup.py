ef723b006923a8bb526620d2815d2207bb4f0567
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-05-08 15:40:49 -0700

    😇 Pass test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tf_lessons",
-    version="0.1.0",
+    version="0.1.1",
     packages=["tf_lessons"],
     author="Ciencia de Datos • GECI",
     python_requires=">=3.9",
--- tf_lessons/__init__.py
+++ tf_lessons/__init__.py
@@ -1,4 +1,4 @@
 """An amazing sample package!"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 from .lesson_1 import *  # noqa
